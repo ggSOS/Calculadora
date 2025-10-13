ed1f3af3ea731032ec5188ca40f4b91ecc18f0cd
GustavoG <126207720+ggSOS@users.noreply.github.com>
2025-10-13 09:57:41 -0300

    Project Update

---
+++ Calculadora.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal abstract class Calculadora
+    {
+        private static double Soma(double x, double y)
+        {
+            return x + y;
+        }
+        private static double Subtracao(double x, double y)
+        {
+            return x - y;
+        }
+        private static double Multiplicacao(double x, double y)
+        {
+            return x * y;
+        }
+        private static double? Divisao(double x, double y)
+        {
+            if (y == 0)
+            {
+                return null;
+            }
+            return x / y;
+        }
+
+        public static double? RealizarOperacao(string operacao, double x, double y)
+        {
+            if (operacao == "+")
+            {
+                return Calculadora.Soma(x, y);
+            }
+            if (operacao == "-")
+            {
+                return Calculadora.Subtracao(x, y);
+            }
+            if (operacao == "x")
+            {
+                return Calculadora.Multiplicacao(x, y);
+            }
+            if (operacao == "+")
+            {
+                return Calculadora.Divisao(x, y);
+            }
+            return null;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System.Drawing;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculadora
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Calculadora calculadora;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            calculadora = new Calculadora( new double[1]);
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {   
+            //double saida = calculadora.Processa();
+            //displaySaida.Text = saida.ToString();
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+            string entrada = input1.Text;
+            // out _ se não precisar do valor convertido
+            if (double.TryParse(entrada, out double entradaConvertida))
+            {
+                calculadora.addValor(entradaConvertida);
+            }
+        }
+
+        private void input1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
