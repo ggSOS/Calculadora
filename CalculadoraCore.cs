84729ab9affc0651815ce5e841bef446680c229e
GustavoG <126207720+OlaMeusAmigues@users.noreply.github.com>
2025-10-15 00:01:41 -0300

    Project Update

---
+++ CalculadoraCore.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class CalculadoraCore
+    {
+        private double[] valores;
+        private int index;
+        public CalculadoraCore()
+        {
+            valores = new double[8];
+            index = 0;
+        }
+
+        public bool AddValor(double valor)
+        {
+            if (index >= valores.Length)
+            {
+                return false;
+            }
+            valores[index] = valor;
+            index++;
+            return true;
+        }
+
+        public double Soma()
+        {
+            double valor = 0;
+
+            foreach (double item in valores)
+            {
+
+                valor += item;
+                Console.WriteLine(item);
+
+            }
+            return valor;
+        }
+
+
+        public double Subtracao()
+        {
+            double valor = 0;
+
+            foreach (double item in valores)
+            {
+
+                valor -= item;
+
+            }
+
+            return valor;
+        }
+
+
+        public double Multiplicacao()
+        {
+            double valor = 1.0;
+
+            for (int count = 0; count < index; count++)
+            {
+                valor *= valores[count];
+            }
+            return valor;
+        }
+
+
+        public double? Divisao()
+        {
+            double valor = 1.0;
+
+            for (int count = 0; count < index; count++)
+            {
+                if (valores[count] == 0 && count > 0)
+                {
+                    return null;
+                }
+                valor /= valores[count];
+            }
+
+            return valor;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,40 +17,88 @@ namespace Calculadora
     /// </summary>
     public partial class MainWindow : Window
     {
-        Calculadora calculadora;
+        CalculadoraCore calculadora = new CalculadoraCore();
 
         public MainWindow()
         {
             InitializeComponent();
 
-            calculadora = new Calculadora( new double[1]);
-
         }
 
-        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        private void textInput_TextChanged(object sender, TextChangedEventArgs e)
         {
 
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {   
-            //double saida = calculadora.Processa();
-            //displaySaida.Text = saida.ToString();
+        private void showInput_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
         }
 
-        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        private void displaySaida_TextChanged(object sender, TextChangedEventArgs e)
         {
-            string entrada = input1.Text;
-            // out _ se não precisar do valor convertido
-            if (double.TryParse(entrada, out double entradaConvertida))
+
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            calculadora = new CalculadoraCore();
+            textInput.Text = "";
+            showInput.Text = "";
+
+        }
+
+        private void adicionarNum_Click(object sender, RoutedEventArgs e)
+        {
+            string entrada = textInput.Text;
+            double valor;
+            if (double.TryParse(entrada, out valor))
             {
-                calculadora.addValor(entradaConvertida);
+                if (calculadora.AddValor(valor))
+                {
+                    textInput.Text = "";
+                    showInput.Text += valor.ToString() + "\n";
+                }
             }
         }
 
-        private void input1_TextChanged(object sender, TextChangedEventArgs e)
-        {
 
+        private void processarSoma_Click(object sender, RoutedEventArgs e)
+        {
+            double saida = calculadora.Soma();
+
+            displaySaida.Text = saida.ToString();
+        }
+
+
+        private void processarSubtracao_Click(object sender, RoutedEventArgs e)
+        {
+            double saida = calculadora.Subtracao();
+
+            displaySaida.Text = saida.ToString();
+        }
+
+
+        private void processarMultiplicacao_Click(object sender, RoutedEventArgs e)
+        {
+            double saida = calculadora.Multiplicacao();
+
+            displaySaida.Text = saida.ToString();
+        }
+
+
+        private void processarDivisao_Click(object sender, RoutedEventArgs e)
+        {
+            double? saida = calculadora.Divisao();
+
+            if (saida == null)
+            {
+                displaySaida.Text = "Nenhum número é divisível por zero";
+            }
+            else
+            {
+                displaySaida.Text = saida.ToString();
+            }
         }
     }
 }
